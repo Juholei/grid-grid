45f1ba774cdade63ddba3117bfb2f94a83c2162a
Juho Leinonen <juholei@gmail.com>
2020-08-22 17:33:30 +0300

    Create Stream component, add all European Grid streams to the front page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Stream from "components/Stream";
+
+const channels = [
+  "bilborl",
+  "dignitas",
+  "endpointtv",
+  "guildesports",
+  "rocketbenelux",
+  "johnnyboi_i",
+  "vitality",
+  "solary",
+  "bdsesport",
+  "teamliquid",
+  "teamsingularitytv",
+  "rams_rl",
+  "bluey",
+  "giants",
+  "ronaky",
+  "lawler"
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header></header>
+      <main>
+        {channels.map((channel) => (
+          <Stream key={channel} channel={channel} parent="localhost" />
+        ))}
+      </main>
     </div>
   );
 }
---
+++ src/components/Stream/Stream.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import styles from "./Stream.module.css";
+
+interface StreamProps {
+  channel: string;
+  parent: string;
+}
+const Stream: React.FC<StreamProps> = ({ channel, parent }) => {
+  return (
+    <div className={styles.stream}>
+      <iframe
+        src={`https://player.twitch.tv/?channel=${channel}&parent=${parent}`}
+        frameBorder="0"
+        allow="autoplay; fullscreen"
+        scrolling="no"
+        title={channel}
+      />
+    </div>
+  );
+};
+
+export default Stream;
