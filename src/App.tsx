5c639b06161bd119c369a70420a301df95d4df3a
Juho Leinonen <juholei@gmail.com>
2020-08-24 19:54:06 +0300

    Add NA channels and buttons to switch between EU and NA, add a heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Stream from "components/Stream";
 import GridContainer from "components/GridContainer";
 
-const channels = [
+const euChannels = [
   "bilborl",
   "dignitas",
   "endpointtv",
@@ -21,10 +21,36 @@ const channels = [
   "lawler"
 ];
 
+const naChannels = [
+  "alpine_esports",
+  "karmaah",
+  "eunited",
+  "athena",
+  "pioneersgg",
+  "nrggg",
+  "spoodah",
+  "knights",
+  "sad_junior",
+  "roguegg",
+  "johnnyboi_i",
+  "soniqsevents",
+  "teamenvy",
+  "reddshope",
+  "version1gg",
+  "xsetgaming"
+];
+
+
 function App() {
+
+  const  [ channels, setChannels ]  = useState(euChannels);
   return (
     <div className="App">
-      <header></header>
+      <header>
+        <h1>Grid Grid</h1>
+        <button onClick={() => setChannels(euChannels)}>EU</button>
+        <button onClick={() => setChannels(naChannels)}>NA</button>
+      </header>
       <main>
         <GridContainer>
         {channels.map((channel) => (
