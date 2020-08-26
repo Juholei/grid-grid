038b5a3dd91d602386af224147528dad521f8eb3
Juholei <juholei@gmail.com>
2020-08-26 13:54:48 +0000

    Deploying to gh-pages from  @ 64d1b14ba6e273823dd94b5657ad5c9cbceffc6c 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8f9cce6df02d8290987bcffd8dd6d832.js"
+  "/grid-grid/precache-manifest.549adc1a64e87aa0aa0e38f30c8dde24.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/grid-grid/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
